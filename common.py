06960053723d3a696418e254a7984b6ebf3e60bb
Jeremy Tandy <jeremy.tandy@gmail.com>
2014-07-03 09:33:52 +0100

    removed ref to specific Vol
    we have a new volume to contend with in WMO No. 306 (Vol I.3) …
    registers from there are also included herein so I removed the
    reference to Vol I.2

--- common.py
+++ common.py
@@ -8,8 +8,8 @@ def writettl():
     with open('ttl/common.ttl', 'w') as fhandle:
         fhandle.write(ttlhead)
         fhandle.write('<common> a reg:Register ;\n')
-        fhandle.write('\trdfs:label "WMO No. 306 Vol I.2 common concepts" ;\n')
-        fhandle.write('\tdc:description "Register of concepts common across WMO No. 306 Vol I.2 formats"@en ;\n')
+        fhandle.write('\trdfs:label "WMO No. 306 common concepts" ;\n')
+        fhandle.write('\tdc:description "Register of concepts common across WMO No. 306 formats"@en ;\n')
         fhandle.write('\treg:owner <http://codes.wmo.int/system/organization/wmo> ;\n')
         fhandle.write('\tdct:publisher <http://codes.wmo.int/system/organization/wmo> ;\n')
         fhandle.write('\treg:manager <http://codes.wmo.int/system/organization/www-dm> ;\n')
